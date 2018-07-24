7b50130856b4a35c7b2ec3900a2b23d810b0d9bc
Deepak Tatineni <depkchowdary@gmail.com>
2018-07-24 06:57:38 +0000

    Added bootstarp, Header components

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">StoX</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#"> Portfolio <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Stocks</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><h6><strong>Funds: $10,000</strong></h6></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> Save & Load <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Save</a></li>
+            <li><a href="#">Load</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+  </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+</style>
